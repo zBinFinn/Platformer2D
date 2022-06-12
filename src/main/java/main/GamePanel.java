e11de90fba656cc3c5aeb2c62cdd8badf21d7ada
zBinFinn <ermert.finn@icloud.com>
2022-06-12 17:25:56 +0200

    + Gravity + Jumping + Collision + Map
    - Up/Down Movement
    
    o Fixed Space between hitbox and ground

---
+++ src/main/java/main/GamePanel.java
@@ -0,0 +1,48 @@
+package main;
+
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static main.Game.GAME_HEIGHT;
+import static main.Game.GAME_WIDTH;
+
+public class GamePanel extends JPanel {
+
+    private MouseInputs mouseInputs;
+    private Game game;
+
+    public GamePanel(Game game) {
+
+        this.game = game;
+        mouseInputs = new MouseInputs(this);
+
+        setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+        setPreferredSize(size);
+        System.out.println("size: " + GAME_WIDTH + " : " + GAME_HEIGHT);
+    }
+
+    public void updateGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        game.render(g);
+    }
+
+    public Game getGame() {
+        return game;
+    }
+}
